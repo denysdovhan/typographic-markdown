9d64893e78afd6ffd8a8a163b6f586fe5f00169f
Denys Dovhan <email@denysdovhan.com>
2015-12-15 17:26:12 +0200

    Siplify index.js.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import mdast from 'mdast';
 import mdtextr from 'mdast-textr';
 import base from 'typographic-base';
 
-export default function typographicMarkdown(text = '', options = {}) {
-  options.plugins = options.plugins || [base];
-  return mdast.use(mdtextr, options).process(text).trim();
+export default function typographicMarkdown(text = '', {
+  options,
+  plugins = [base]
+} = {}) {
+  return mdast.use(mdtextr, { options, plugins }).process(text).trim();
 };
