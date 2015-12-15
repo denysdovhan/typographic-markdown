98bb1bc8e5d5d51a9ec25271a1fd076a30629835
Denys Dovhan <email@denysdovhan.com>
2015-12-16 00:06:58 +0200

    Small change in API.

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import mdast from 'mdast';
 import mdtextr from 'mdast-textr';
 import base from 'typographic-base';
 
-export default function typographicMarkdown(text = '', {
-  options,
-  plugins = [base]
-} = {}) {
+export default function typographicMarkdown(
+  text = '',
+  options = { locale: 'en-us' },
+  plugins = [ base ]
+) {
   return mdast.use(mdtextr, { options, plugins }).process(text).trim();
 };
--- test.js
+++ test.js
@@ -2,9 +2,9 @@ import { equal } from 'assert';
 import t from './index';
 
 it('should pass options', () =>
-  equal(t('_"quotes"_', { options: { locale: 'uk' }}), '_«quotes»_')
+  equal(t('_"quotes"_', { locale: 'uk' }), '_«quotes»_')
 );
 
 it('should pass plugins', () =>
-  equal(t('**ellipses**...', { plugins: ['typographic-ellipses'] }), '**ellipses**…')
+  equal(t('**"quotes"**', { locale: 'en-us' }, ['typographic-quotes']), '**“quotes”**')
 );
