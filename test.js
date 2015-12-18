98c5410727633f840972b262caeb3d4149935fbc
Denys Dovhan <email@denysdovhan.com>
2015-12-18 18:00:08 +0200

    Fix tests.

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
 import { equal } from 'assert';
 import t from './index';
+import quotes from 'typographic-quotes';
 
 it('should pass options', () =>
   equal(t('_"quotes"_', { locale: 'uk' }), '_«quotes»_')
 );
 
 it('should pass plugins', () =>
-  equal(t('**"quotes"**', ['typographic-quotes']), '**“quotes”**')
+  equal(t('**"quotes"**...', [quotes]), '**“quotes”**...')
 );
